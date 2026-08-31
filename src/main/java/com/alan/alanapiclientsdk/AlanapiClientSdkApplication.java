a58bf92b791172b23911ceb2cc621d33f7a83847
yangsaichao <alanysc@qq.com>
2026-08-31 23:03:47 +0800

    Initial commit

---
+++ src/main/java/com/alan/alanapiclientsdk/AlanapiClientSdkApplication.java
@@ -0,0 +1,13 @@
+package com.alan.alanapiclientsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlanapiClientSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlanapiClientSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alan/alanapiclientsdk/AlanapiClientSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.alanapiclientsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlanapiClientSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
